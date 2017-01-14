78ad31675731e40f9648497bb25d7afb57501f6b
Mojtaba Dashtinejad <mojtaba@gmail.com>
2017-01-14 13:49:59 +0330

    incomplete function

--- csv.js
+++ csv.js
@@ -8,28 +8,83 @@
  * @param quote String, single character used to quote non-simple fields.
  *        Defaults to "\"".
  */
-export default function (input, separator, quote) {
+export default function parseCSV(input, separator, quote) {
   separator = separator || ','
   quote = quote || '"'
   
   // the final return array which contains arrays (for each row)
   let result = [
-    []
+    [ ]
   ]
 
+  // this flag determines if we are on quoted string or not
+  let inQuote = false
+
   // loop through each character of input
+  let value = []
+  let row = result[ result.length - 1 ]
+
   for (let i = 0; i < input.length; i++) {
     // save the character inside a constant
     const char = input[i]
 
-    // we put the values of current row inside an array
-    let row = result[ result.length - 1 ]
+    if (! inQuote) {
+      if (char == quote) {
+        inQuote = true
+        continue
+      }
 
-    // if the character isn't separator, add it to the current row
-    if (char != separator) {
-      row.push(char)
+      // check for newlines
+      if (char == '\n') {
+        row.push( value.join('') )
+        value = []
+
+        result.push([])
+        row = result[ result.length - 1 ]
+      }
+
+      else {
+        // if the character isn't separator, add it to the current row
+        if (char != separator) {
+          value.push(char)
+        }
+
+        // we faced with separator
+        else {
+          row.push( value.join('') )
+          value = []
+        }
+      }
+    }
+    
+    // we faced with quoted string
+    else {
+      if (char == quote) {
+        if (typeof input[i + 1] != typeof undefined) {
+          if (input[i + 1] == quote) {
+            value.push(quote)
+          }
+        }
+
+        inQuote = false
+      }
+      
+      else {
+        // if the character isn't separator, add it to the current row
+        if (char != separator) {
+          value.push(char)
+        }
+
+        // we faced with separator
+        else {
+          row.push( value.join('') )
+          value = []
+        }
+      }
     }
   }
+
+  row.push( value.join('') )
   
   return result
 }
--- test.js
+++ test.js
@@ -1,24 +1,107 @@
 import parseCSV from './csv'
 
-describe('Tests', () => {
-  it("empty", () => {
-    const input = ""
-    const output = [[]]
 
-    expect(parseCSV(input)).toMatchObject(output);
+describe('Basic CSV Functionality', () => {
+  it('empty', () => {
+    const input = ''
+    const output = [['']]
+
+    expect(parseCSV(input)).toMatchObject(output)
   })
 
-  it("single value", () => {
-    const input = "1"
+  it('empty values', () => {
+    const input = ',,'
+    const output = [['', '', '']]
+
+    expect(parseCSV(input)).toMatchObject(output)
+  })
+
+  it('single value', () => {
+    const input = '1'
     const output = [['1']]
 
-    expect(parseCSV(input)).toMatchObject(output);
+    expect(parseCSV(input)).toMatchObject(output)
   })
 
-  it("multi values", () => {
-    const input = "1,2,3"
+  it('single complex value', () => {
+    const input = 'one'
+    const output = [['one']]
+
+    expect(parseCSV(input)).toMatchObject(output)
+  })
+
+  it('multi values', () => {
+    const input = '1,2,3'
     const output = [['1', '2', '3']]
 
-    expect(parseCSV(input)).toMatchObject(output);
+    expect(parseCSV(input)).toMatchObject(output)
+  })
+
+  it('multi complex values', () => {
+    const input = 'one,two,3,four'
+    const output = [['one', 'two', '3', 'four']]
+
+    expect(parseCSV(input)).toMatchObject(output)
+  })
+
+  it('multi lines', () => {
+    const input = '1\n2\n3'
+    const output = [['1'], ['2'], ['3']]
+
+    expect(parseCSV(input)).toMatchObject(output)
+  })
+
+  it('multi complex lines', () => {
+    const input = '1,2,three\none\ntwo\n3\nfour'
+    const output = [['1', '2', 'three'], ['one'], ['two'], ['3'], ['four']]
+
+    expect(parseCSV(input)).toMatchObject(output)
+  })
+})
+
+describe('Quoted Values', () => {
+  it('simple quote', () => {
+    const input = '"one"'
+    const output = [["one"]]
+
+    expect(parseCSV(input)).toMatchObject(output)
+  })
+
+  it('simple multi quote values', () => {
+    const input = '"one",2,"three"'
+    const output = [["one", '2', 'three']]
+
+    expect(parseCSV(input)).toMatchObject(output)
+  })
+
+  it('quote with new lines', () => {
+    const input = '"one"\n"two"'
+    const output = [['one'], ['two']]
+
+    expect(parseCSV(input)).toMatchObject(output)
+  })
+
+  it('multi quote with new lines', () => {
+    const input = '1,"two",3,"four"\n"one",2,"three",4'
+    const output = [['1', 'two', '3', 'four'], ['one', '2', 'three', '4']]
+
+    expect(parseCSV(input)).toMatchObject(output)
+  })
+
+  it('quote with new lines inside', () => {
+    const input = '"one\ntwo"'
+    const output = [['one\ntwo']]
+
+    expect(parseCSV(input)).toMatchObject(output)
+  })
+})
+
+
+describe('Nested Quoted Values', () => {
+  it('simple nested quote', () => {
+    const input = '"one \"\"two\"\" three'
+    const output = [['one "two" three']]
+
+    expect(parseCSV(input)).toMatchObject(output)
   })
 })
